d3462b6ec8ad2aa5431072ca0a74fdd2a6bb4d60
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-01-04 11:21:24 +0600

    set up another react app for js

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,15 +1,23 @@
 import { Button } from "@mui/material";
 import React, { FC } from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter } from "react-router-dom";
+import Homepage from "./components/Homepage";
 
 function App() {
-  const router = createBrowserRouter([{ path: "*", Component: UserRoutes }]);
+  // const router = createBrowserRouter([{ path: "*", Component: UserRoutes }]);
   return (
     <>
       {/* <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button> */}
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+      {/* <BrowserRouter> */}
+      <Routes>
+
+      <Route path="/home" element={<Homepage />} />
+        </Routes>
+      </Routes>
+    {/* </BrowserRouter> */}
     </>
   );
 }
