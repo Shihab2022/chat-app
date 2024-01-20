2447758b3bb29c6374d0226a4a7d1c7d6a46a37c
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-01-20 21:16:10 +0600

    create component

---
+++ front-end/src/components/profile.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import AddIcon from "@mui/icons-material/Add";
+import { useNavigate } from "react-router-dom";
+export default function Profile({ onClick, user }) {
+  const navigate = useNavigate();
+  const { name, lastMessage, img, time } = user;
+  return (
+    <Card
+      sx={{
+        display: "flex",
+        direction: "row",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        spacing: 2,
+        width: "100%",
+        paddingLeft: "20px",
+        cursor: "pointer",
+      }}
+    >
+      <Avatar alt="Cindy Baker" src={img} />
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CardContent sx={{ flex: "1 0 auto" }}>
+          <Typography component="div" variant="h6">
+            {name}
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            color="text.secondary"
+            component="div"
+          >
+            {lastMessage}
+          </Typography>
+        </CardContent>
+      </Box>
+      <AddIcon onClick={() => navigate("/inviteUser", { state: { user } })} />
+    </Card>
+  );
+}
