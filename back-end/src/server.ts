be31901dc904db535af6e6daf72bd39131293e89
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-07-23 17:38:41 +0600

    handle server error

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,14 +1,31 @@
 import mongoose from "mongoose";
 import app from "./app"
 import config from "./app/config"
-
+import { Server } from "http"
 async function main() {
     try {
         await mongoose.connect(config.database_url as string);
-
-        app.listen(config.port, () => {
-            console.log(`App listening on port ${config.port}`);
+        const port = config.port
+        const server: Server = app.listen(port, () => {
+            console.log(`Chat app server is running on  ${port}`)
+        })
+        const exitHandler = () => {
+            if (server) {
+                server.close(() => {
+                    console.info("Server closed!")
+                })
+            }
+            process.exit(1);
+        };
+        process.on('uncaughtException', (error) => {
+            console.log(error);
+            exitHandler();
         });
+
+        process.on('unhandledRejection', (error) => {
+            console.log(error);
+            exitHandler();
+        })
     } catch (error) {
         console.log('error', error);
     }
