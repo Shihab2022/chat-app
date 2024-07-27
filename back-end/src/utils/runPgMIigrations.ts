db225bf4d2e6eb25ffea97a8bee9d2cd42ce2888
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-07-27 14:43:34 +0600

    put code on try catch block

--- back-end/src/utils/runPgMIigrations.ts
+++ back-end/src/utils/runPgMIigrations.ts
@@ -3,25 +3,29 @@ import { pool } from "./postgres";
 import path from "path";
 
 export const runPgMigrations = async () => {
-    // Code to run migrations
-    const Postgrator = await import("postgrator");
-    const postgrator = new Postgrator.default({
-        migrationPattern: path.join(__dirname, "/migrations/*"),
-        driver: "pg",
-        database: config.pg_info.pg_db_url,
-        schemaTable: "schemaversion",
-        execQuery: (query: any) => pool.query(query),
-    });
-    postgrator.on("migration-started", (migration: any) =>
-        console.log({ Starting: migration })
-    );
-    postgrator.on("migration-finished", (migration: any) =>
-        console.log({ Completed: migration })
-    );
-    await postgrator.migrate();
-    const cuurentDbVersion = await postgrator.getDatabaseVersion();
-    const allMigrations = await postgrator.getMigrations();
-    await postgrator.runMigrations(allMigrations.slice(cuurentDbVersion));
+    try {
+        // Code to run migrations
+        const Postgrator = await import("postgrator");
+        const postgrator = new Postgrator.default({
+            migrationPattern: path.join(__dirname, "/migrations/*"),
+            driver: "pg",
+            database: config.pg_info.pg_db_url,
+            schemaTable: "schemaversion",
+            execQuery: (query: any) => pool.query(query),
+        });
+        postgrator.on("migration-started", (migration: any) =>
+            console.log({ Starting: migration })
+        );
+        postgrator.on("migration-finished", (migration: any) =>
+            console.log({ Completed: migration })
+        );
+        await postgrator.migrate();
+        const cuurentDbVersion = await postgrator.getDatabaseVersion();
+        const allMigrations = await postgrator.getMigrations();
+        await postgrator.runMigrations(allMigrations.slice(cuurentDbVersion));
 
-    console.log("All migration run successfully!!!!!!! ");
+        console.log("All migration run successfully!!!!!!! ");
+    } catch (error) {
+        console.log({ error })
+    }
 }
