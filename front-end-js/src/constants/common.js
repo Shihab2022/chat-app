68754cb552600d5393af862a669a42b0ca24f41c
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-07-30 07:43:31 +0600

    delete js front end

--- front-end-js/src/constants/common.js
+++
@@ -1,6 +0,0 @@
-export const SUCCESS = "success";
-export const WARNING = "warning";
-export const FAILED = "failed";
-export const INFO = "info";
-
-export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
