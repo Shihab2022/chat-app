6ba31ab4c1d97c5f94e0429f94d317a9e7845860
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-08-20 13:53:05 +0600

    update date format

--- front-end/src/utils/timeFormat.ts
+++ front-end/src/utils/timeFormat.ts
@@ -1,14 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export const formatDate: any = (isoString: string) => {
-  const dateObj = new Date(isoString);
-  const day = String(dateObj.getDate()).padStart(2, "0");
-  const month = String(dateObj.getMonth() + 1).padStart(2, "0"); // months are 0-indexed
-  const year = dateObj.getFullYear();
-  const weekday = dateObj.toLocaleDateString("en-US", { weekday: "long" });
 
-  const formattedDate = `${day}/${month}/${year} (${weekday})`;
+export const formatDate = (isoString: Date): string => {
+  const d = new Date(isoString);
+  const today = new Date();
+  const toLocalDateOnly = (dt: Date) =>
+    new Date(dt.getFullYear(), dt.getMonth(), dt.getDate());
+  const dOnly = toLocalDateOnly(d);
+  const todayOnly = toLocalDateOnly(today);
 
-  return formattedDate;
+  const msPerDay = 24 * 60 * 60 * 1000;
+  const diffInDays = Math.round(
+    (todayOnly.getTime() - dOnly.getTime()) / msPerDay
+  );
+
+  if (diffInDays === 0) return "Today";
+  if (diffInDays === 1) return "Yesterday";
+  if (diffInDays >= 2 && diffInDays <= 6) {
+    return d.toLocaleDateString("en-US", { weekday: "long" });
+  }
+  const day = String(d.getDate()).padStart(2, "0");
+  const month = String(d.getMonth() + 1).padStart(2, "0");
+  const year = d.getFullYear();
+  const weekday = d.toLocaleDateString("en-US", { weekday: "long" });
+
+  return `${day}/${month}/${year} (${weekday})`;
 };
 
 export const formatTimes: any = (isoString: any) => {
