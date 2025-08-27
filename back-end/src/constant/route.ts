33d4b1e57a0bbe9c806679e850dc5fd6032317b0
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-08-27 22:50:55 +0600

    fix run time issue

--- back-end/src/constant/route.ts
+++ back-end/src/constant/route.ts
@@ -1,25 +1,24 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 // import httpStatus from "http-status";
-import { appName } from ".";
-import { pgRequest } from "../utils/postgres";
+import { appName } from '.';
 export const testingRoute = async (req: Request, res: Response) => {
-    const query = 'Select * from users'
-    const responce = await pgRequest(query)
-    res.send({
-        message: `Hi Guys, Welcome to ${appName} Server !`,
-        responce
-    });
+  const query = 'Select * from users';
+  res.send({
+    message: `Hi Guys, Welcome to ${appName} Server !`,
+  });
 };
 
-
-export const notFound = (req: Request, res: Response, next: NextFunction): any => {
-    return res.status(404).json({
-        success: false,
-        message: "Api not found ",
-        error: {
-            // path: req.originUrl,
-            message: "Your requested path is  not found"
-
-        }
-    })
-}
+export const notFound = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): any => {
+  return res.status(404).json({
+    success: false,
+    message: 'Api not found ',
+    error: {
+      // path: req.originUrl,
+      message: 'Your requested path is  not found',
+    },
+  });
+};
