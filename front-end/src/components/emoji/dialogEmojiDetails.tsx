5419e4bce4dcc2d5369420adda333a8b24437d70
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-09-07 00:29:35 +0600

    create basic card for showing reactions details

--- front-end/src/components/emoji/dialogEmojiDetails.tsx
+++ front-end/src/components/emoji/dialogEmojiDetails.tsx
@@ -9,7 +9,16 @@ import DialogTitle from "@mui/material/DialogTitle";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { SET_EMOJI_DETAILS_DIALOG_STATUS } from "../../redux/features/chat/getConversationSlice";
-import { IconButton, Stack, styled, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  IconButton,
+  Stack,
+  styled,
+  Typography,
+} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import Divider from "@mui/material/Divider";
 import { formateEmojiDialogData } from "../../utils/common";
@@ -23,7 +32,66 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
     padding: theme.spacing(1),
   },
 }));
+const CCard = ({ formattedData, selectedEmoji }: any) => {
+  const data = get(formattedData, selectedEmoji, []);
+  return (
+    <>
+      {data.map((d: any) => {
+        const { emoji, img, name, _id, userId } = d;
+        console.log({ d });
+        return (
+          <Stack
+            direction="row"
+            spacing={4}
+            sx={{
+              justifyContent: "space-between",
+              alignItems: "center",
+              minWidth: "500px",
+            }}
+          >
+            <Card
+              elevation={0}
+              sx={{
+                display: "flex",
+                direction: "row",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                spacing: 2,
+                width: "100%",
+                paddingLeft: "20px",
+                cursor: "pointer",
+              }}
+              //   onClick={() => onClick(use r)}
+            >
+              <Avatar
+                alt={name}
+                src={img || `https://randomuser.me/api/portraits/men/3.jpg`}
+              />
 
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <CardContent sx={{ flex: "1 0 auto" }}>
+                  <Typography component="div" variant="h6">
+                    {name}
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    component="div"
+                    sx={{ fontSize: "12px" }}
+                  >
+                    Click to view profile
+                  </Typography>
+                </CardContent>
+              </Box>
+            </Card>
+
+            <Typography variant="h4">{emoji}</Typography>
+          </Stack>
+        );
+      })}
+    </>
+  );
+};
 export default function EmojiDetailsDialog() {
   const [selectedEmoji, setSelectedEmoji] = useState("All");
   const { emojiDetailsDialogStatus, selectedReactions = [] } = useSelector(
@@ -74,23 +142,24 @@ export default function EmojiDetailsDialog() {
             sx={{
               justifyContent: "flex-start",
               alignItems: "center",
+              marginBottom: "20px",
             }}
           >
             {Object.keys(formattedData).map((a) => {
               const v = get(formattedData, a, []);
               return (
                 <Typography
-                  onClick={() =>
-                    dispatch(SET_EMOJI_DETAILS_DIALOG_STATUS(false))
-                  }
+                  onClick={() => setSelectedEmoji(a)}
                   sx={{ cursor: "pointer" }}
-                  variant="h6"
+                  variant="h5"
                 >
                   {a} {v?.length}
                 </Typography>
               );
             })}
           </Stack>
+
+          <CCard formattedData={formattedData} selectedEmoji={selectedEmoji} />
         </DialogContent>
       </BootstrapDialog>
     </>
