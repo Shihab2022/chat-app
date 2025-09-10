b0eeef60c6b82f49ea99595c897c1662ee804549
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-09-10 20:01:35 +0600

    create basic all option for action

--- front-end/src/components/messages/moreAction.tsx
+++ front-end/src/components/messages/moreAction.tsx
@@ -1,7 +1,63 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
+import ReplyIcon from "@mui/icons-material/Reply";
+import { Box, Stack, Typography } from "@mui/material";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import DeleteIcon from "@mui/icons-material/Delete";
 
+const config = [
+  {
+    id: 1,
+    title: "Reply",
+    icon: <ReplyIcon />,
+  },
+  {
+    id: 34442,
+    title: "Copy",
+    icon: <ContentCopyIcon />,
+  },
+  {
+    id: 3341,
+    title: "React",
+    icon: <InsertEmoticonIcon />,
+  },
+  {
+    id: 134,
+    title: "Forward",
+    icon: <ArrowForwardIcon />,
+  },
+  {
+    id: 1444,
+    title: "Delete",
+    icon: <DeleteIcon />,
+  },
+];
+const CCard = ({ c, handleClick }: any) => {
+  const { icon, title } = c;
+  return (
+    <>
+      <Box sx={{ minWidth: "200px" }}>
+        <Stack
+          direction="row"
+          spacing={1}
+          sx={{
+            justifyContent: "flex-start",
+            alignItems: "center",
+            paddingY: "10px",
+            paddingX: "15px",
+            cursor: "pointer",
+          }}
+          onClick={() => handleClick(title)}
+        >
+          {icon}
+          <Typography>{title}</Typography>
+        </Stack>
+      </Box>
+    </>
+  );
+};
 export default function MoreActions({
   setMoreActionAnchorEl,
   setMoreActionOpen,
@@ -12,7 +68,9 @@ export default function MoreActions({
     setMoreActionAnchorEl(null);
     setMoreActionOpen(false);
   };
-
+  const handleClick = (v: any) => {
+    console.log(v);
+  };
   return (
     <>
       <Menu
@@ -21,9 +79,11 @@ export default function MoreActions({
         open={moreActionOpen}
         onClose={handleClose}
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        {config.map((c) => (
+          <CCard c={c} onClick={handleClick}>
+            Profile
+          </CCard>
+        ))}
       </Menu>
     </>
   );
