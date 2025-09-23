093d1cb5fd4e4e3450bec61447487d1be544c057
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-09-23 17:43:34 +0600

    create basic ui for forward message

--- front-end/src/components/messages/forwardMessage.tsx
+++ front-end/src/components/messages/forwardMessage.tsx
@@ -7,7 +7,15 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 import { TransitionProps } from "@mui/material/transitions";
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import Stack from "@mui/material/Stack/Stack";
+import Avatar from "@mui/material/Avatar/Avatar";
+import Typography from "@mui/material/Typography/Typography";
+import { Box, Checkbox } from "@mui/material";
+import { all } from "axios";
+import { TUser } from "../../types";
 
 const Transition = forwardRef(function Transition(
   props: TransitionProps & {
@@ -18,6 +26,15 @@ const Transition = forwardRef(function Transition(
   return <Slide direction="up" ref={ref} {...props} />;
 });
 const ForwardMessage = ({ mess, forwardMenuOpen, setForwardMenuOpen }: any) => {
+  const { loginUser, allUsers } = useSelector(
+    (state: RootState) => state?.auth
+  );
+  const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
+  const handleToggle = (id: string) => {
+    setSelectedUsers((prev) =>
+      prev.includes(id) ? prev.filter((uid) => uid !== id) : [...prev, id]
+    );
+  };
   return (
     <>
       <Dialog
@@ -27,10 +44,46 @@ const ForwardMessage = ({ mess, forwardMenuOpen, setForwardMenuOpen }: any) => {
         onClose={() => setForwardMenuOpen(false)}
         aria-describedby="alert-dialog-slide-description"
       >
-        <DialogTitle>Are you sure you want to delete this message?</DialogTitle>
+        <DialogTitle>Forward Message to your friends</DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-slide-description">
-            {mess?.text}
+            {allUsers
+              .filter((user: TUser) => user._id !== loginUser._id)
+              .map((user: TUser) => {
+                const { _id, name, img } = user;
+                return (
+                  <Stack
+                    direction="row"
+                    spacing={2}
+                    sx={{
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      height: "100%",
+                      color: "#000",
+                      marginY: "10px",
+                    }}
+                  >
+                    <Stack
+                      direction="row"
+                      spacing={2}
+                      sx={{
+                        justifyContent: "flex-start",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Avatar alt={name} src={img} />
+
+                      <Typography component="div" variant="body1">
+                        {name}
+                      </Typography>
+                    </Stack>
+                    <Checkbox
+                      checked={selectedUsers.includes(_id)}
+                      onChange={() => handleToggle(_id)}
+                    />
+                  </Stack>
+                );
+              })}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
