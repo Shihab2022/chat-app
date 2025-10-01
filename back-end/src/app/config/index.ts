8d2e75444eee6bfca099683d59d3f0541b746ed7
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-10-01 19:37:03 +0600

    remove  some config file

--- back-end/src/app/config/index.ts
+++ back-end/src/app/config/index.ts
@@ -14,13 +14,13 @@ export default {
   //   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
   jwt_access_expire_in: process.env.JWT_ACCESS_EXPIRES_IN,
   //   jwt_refresh_expire_in: process.env.JWT_REFRESH_EXPIRES_IN,
-  pg_info: {
-    pg_db_user: process.env.PG_DB_USER,
-    pg_db_host: process.env.PG_DB_HOST,
-    pg_db_url: process.env.PG_DB,
-    pg_db_pass: process.env.PG_DB_PASSWORD,
-    pg_db_port: process.env.PG_DB_PORT,
-  },
+  // pg_info: {
+  //   pg_db_user: process.env.PG_DB_USER,
+  //   pg_db_host: process.env.PG_DB_HOST,
+  //   pg_db_url: process.env.PG_DB,
+  //   pg_db_pass: process.env.PG_DB_PASSWORD,
+  //   pg_db_port: process.env.PG_DB_PORT,
+  // },
   cloudinary: {
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
