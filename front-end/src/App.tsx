bdb3ac4bcef3165c11956f8cef4261222480833c
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-10-21 22:10:51 +0600

    fix toast issue

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,17 +1,11 @@
 import { Toaster } from "react-hot-toast";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import UserRoutes from "./routes/index";
-import { NAV_BAR_HEIGHT } from "./constants/common";
 const router = createBrowserRouter([{ path: "*", Component: UserRoutes }]);
 function App() {
   return (
     <>
-      <Toaster
-        toastOptions={{ duration: 5000 }}
-        containerStyle={{
-          top: NAV_BAR_HEIGHT + 5,
-        }}
-      />
+      <Toaster toastOptions={{ duration: 5000 }} />
       <RouterProvider router={router} />
     </>
   );
